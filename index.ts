910c03a254c4488400d99f52134c6d83966c83b5
Carlo <car.passinfo@gmail.com>
2024-06-13 22:35:21 +0800

    get coin data from coingeko api

--- index.ts
+++ index.ts
@@ -1,19 +1,40 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
-// configures dotenv to work in your application
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (request: Request, response: Response) => { 
-  response.status(200).send("Hello World");
-}); 
+const fetchCryptoPrices = async () => {
+  try {
+    const ids = "bitcoin,ethereum,dogecoin";
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=eur`;
 
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  // gracefully handle error
-  throw new Error(error.message);
+    // Fetch the data from CoinGecko API
+    const response = await fetch(url);
+    const prices = await response.json();
+
+    // Log the prices to the console
+    console.log("Bitcoin (BTC) price in EUR:", prices.bitcoin.eur);
+    console.log("Ethereum (ETH) price in EUR:", prices.ethereum.eur);
+    console.log("Dogecoin (DOGE) price in EUR:", prices.dogecoin.eur);
+  } catch (error) {
+    console.error("Error fetching cryptocurrency prices:", error);
+  }
+};
+
+fetchCryptoPrices();
+setInterval(fetchCryptoPrices, 60000);
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send("Hello World");
 });
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
